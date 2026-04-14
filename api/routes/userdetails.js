b2d06a973e55a6f3cd4ff038f616964cc4b98f09
rumana <kadri.rumana@gmail.com>
2026-04-14 16:25:28 +0100

    Fixed issue for userData and added model logic for Profile Get

--- api/routes/userdetails.js
+++ api/routes/userdetails.js
@@ -3,7 +3,6 @@ const { Router } = require('express');
 const userController = require('../controller/userdetails');
 
 const userRouter = Router();
-
 userRouter.post("/create", userController.createUser);
 userRouter.post("/login", userController.getUserDetailsbyEmail);
 
